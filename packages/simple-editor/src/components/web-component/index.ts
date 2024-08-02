9cb6d014719ed91938a64883def74ce123ea72c6
ali ghasami <alighassami@yahoo.com>
2024-08-02 13:21:06 +0330

    feat back link

--- packages/simple-editor/src/components/web-component/index.ts
+++ packages/simple-editor/src/components/web-component/index.ts
@@ -2,13 +2,13 @@
 import wrapper from 'vue3-webcomponent-wrapper'
 import { createApp, h } from 'vue'
 import SelectHintType from './SelectHintType/SelectHintType.vue'
-import DatePicker from '@/components/web-component/DatePicker.vue'
-import Test from '@/components/web-component/Test.vue'
-import Test2 from '@/components/web-component/Test2.vue'
+import DatePicker from './DatePicker.vue'
+import Test from './Test.vue'
+import Test2 from './Test2.vue'
 const components = {
   'select-hint-type': SelectHintType,
   'mahdaad-date-picker': DatePicker,
-  'mahdaad-object-picker-component':Test,
+  //'mahdaad-object-picker-component':Test,
   'mahdaad-object-link-component':Test2
 }
 
