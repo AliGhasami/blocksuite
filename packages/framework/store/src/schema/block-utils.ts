3c554e1989780fd2dfbc86d469e611c4826aee62
h.emami <h.emami>
2025-04-05 16:47:34 +0330

    set direction automatically on callout

--- packages/framework/store/src/schema/block-utils.ts
+++ packages/framework/store/src/schema/block-utils.ts
@@ -13,13 +13,16 @@ export function setDirectionBasedOnText(model: ParagraphBlockModel, doc: any, ke
 export function setDirectionOnBlock(model: ParagraphBlockModel, doc: any, text: string): void {
   if(!text) return
   text = text.replace(/^[^a-zA-Z0-9آ-ی۰-۹]+/, '');
-  //console.log("text:", text);
   const rtlChars = /[\u0590-\u05FF\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF]/;
   const newDir = rtlChars.test(text[0]) ? 'rtl' : 'ltr';
-
+  const parent = doc.getParent(model);
+  
   if (text.length != 0) {
     if (model.dir != newDir) {
       doc.updateBlock(model, { dir: newDir });
+      if (parent.flavour === "affine:mahdaad-callout") {
+        doc.updateBlock(model.parent, { dir: parent.children[0].dir })
+      }
     }
   }
   else {
