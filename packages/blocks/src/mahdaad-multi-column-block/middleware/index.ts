26e997bb4d13c4c462b1ab1a480ad237fe56144f
alighasami <alighassami@yahoo.com>
2025-04-12 19:07:26 +0330

    fix : drop in multicolumn

--- packages/blocks/src/mahdaad-multi-column-block/middleware/index.ts
+++ packages/blocks/src/mahdaad-multi-column-block/middleware/index.ts
@@ -8,12 +8,11 @@ import { checkParentIs, getParent } from "../../_common/mahdaad/is.js";
 export const mahdaadMultiColumnMiddleware: JobMiddleware = ({slots,collection}) => {
   slots.beforeImport.on(payload => {
     if(payload && payload.type=='slice') {
-      console.log("this is payload",payload);
       const doc= collection.getDoc(payload.snapshot.pageId)
       if(doc) {
         const block =doc.getBlock(payload?.parent)
         if(block && (block.flavour==MahdaadMultiColumnBlockSchema.model.flavour ||  checkParentIs(block.model,MahdaadMultiColumnBlockSchema.model.flavour))) {
-          const includeColumn=payload.snapshot.content.filter((item=> [MahdaadMultiColumnBlockSchema.model.flavour].includes(item.flavour)))
+          const includeColumn=payload.snapshot.content.find((item=> [MahdaadMultiColumnBlockSchema.model.flavour].includes(item.flavour)))
           payload.snapshot.content=payload.snapshot.content.filter(item=> ![MahdaadMultiColumnBlockSchema.model.flavour].includes(item.flavour))
           if(includeColumn) {
             const selectedBlock= block.flavour==MahdaadMultiColumnBlockSchema.model.flavour  ? block : getParent(block.model,MahdaadMultiColumnBlockSchema.model.flavour)
